322665e9bab31ee8641c71ba7805be8f112528b4
Félix-Antoine Cassidy-Roy <fa@linsolite.ca>
2021-10-01 15:12:10 -0400

    Alien point 1

---
+++ Assets/Scripts/Alien.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class alien : MonoBehaviour
+{
+    [SerializeField] private float maxHealthPoints = 1;
+
+    private float healthPoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void looseHealth(float points = 1)
+    {
+        maxHealthPoints -= points;
+    }
+
+    void die()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "bullet")
+        {
+            looseHealth();
+
+            if (maxHealthPoints <= 0)
+            {
+                die();
+            }
+        }
+
+        else if (collision.gameObject.tag == "player")
+        {
+            die();
+        }
+    }
+
+    private void OnEnable()
+    {
+        healthPoints = maxHealthPoints;
+    }
+}
