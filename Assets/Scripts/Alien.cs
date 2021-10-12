5cc50cc9eb6ad7005deb1787baf44a26b6dbdba7
Félix-Antoine Cassidy-Roy <fa@linsolite.ca>
2021-10-12 12:04:48 -0400

    Bullet does wlblwlbllwkgl

--- Assets/Scripts/Alien.cs
+++ Assets/Scripts/Alien.cs
@@ -6,18 +6,32 @@ using UnityEngine.AI;
 public class Alien : MonoBehaviour
 {
     private NavMeshAgent navMeshAgent;
+    private Rigidbody alienRigidbody;
     private GameObject player;
 
     // Start is called before the first frame update
     void Start()
     {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        alienRigidbody = GetComponent<Rigidbody>();
         player = GameObject.FindGameObjectWithTag("Player");
-        navMeshAgent = (NavMeshAgent)GetComponent("NavMeshAgent");
+
+        navMeshAgent.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        navMeshAgent.SetDestination(player.transform.position);
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // On active le navMeshAgent quand on touche au sol.
+        if (!navMeshAgent.enabled && collision.gameObject.CompareTag("Terrain"))
+        {
+            navMeshAgent.enabled = true;
+            navMeshAgent.SetDestination(player.transform.position);
+        }
     }
 }
