bfbdd88db7df056abc32fd07bf46d29bd9bb1b3c
Newsupernat <43003974+GrondinNathanael@users.noreply.github.com>
2021-10-15 14:31:46 -0400

    Petite pause avant le son de victoire

--- Assets/Scripts/VictorySoundTrigger.cs
+++ Assets/Scripts/VictorySoundTrigger.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class VictorySoundTrigger : MonoBehaviour
 {
+    [SerializeField] private AudioSource music;
     [SerializeField] private AudioSource victorySound;
+
+    private const float TIME_TO_PAUSE = 3f;
+    private float timeLeft = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,11 +18,20 @@ public class VictorySoundTrigger : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(timeLeft > 0)
+        {
+            timeLeft -= Time.deltaTime;
+            if(timeLeft <= 0)
+            {
+                music.Play();
+            }
+        }
     }
 
     public void playVictorySound()
     {
+        music.Pause();
         victorySound.PlayOneShot(victorySound.clip);
+        timeLeft = TIME_TO_PAUSE;
     }
 }
